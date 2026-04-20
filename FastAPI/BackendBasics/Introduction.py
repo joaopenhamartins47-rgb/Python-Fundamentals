6b5d26f7c672a354f6153f16540e1ceb04dd8bcb
João Vitor Penha <joao.penha.martins47@gmail.com>
2026-04-20 18:19:24 -0300

    post, put and delete methods

--- FastAPI/BackendBasics/Introduction.py
+++ FastAPI/BackendBasics/Introduction.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 
 app = FastAPI()
 
@@ -40,6 +40,36 @@ async def read_author_query(book_author: str, category: str):
             books_to_return.append(book)
 
     return books_to_return
+
+
+#Post request methods
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
+
+#Put request methods
+@app.put("/books/update_book")
+async def update_book(updated_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
+            BOOKS[i] = updated_book
+
+#Delete request methods
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title: str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            break
+
+#API endpoint that can fetch all books from a specific author using path parameters
+@app.get("/books/search/{author}")
+async def search_from_author(author: str):
+    author_books = []
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('author').casefold() == author.casefold():
+            author_books.append(BOOKS[i])
+    return author_books
 
 
 
